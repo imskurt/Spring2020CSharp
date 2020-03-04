68aecdd685703496d28516b6ca955122ede741a2
Mariah Curtis <mariah.s.curtis@gmail.com>
2020-03-03 19:04:32 -0500

    Completed Challenge 2!

---
+++ src/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Mariah.CodeLou.ExerciseProject.Classes;
+using System.Linq;
+
+namespace Mariah.CodeLou.ExerciseProject
+{
+    class Program
+    {
+        static List<Student> studentsList = new List<Student>();
+        static void Main(string[] args)
+        {
+
+            var inputtingStudent = true;
+
+            while (inputtingStudent)
+            {
+                DisplayMenu();
+                var option = Console.ReadLine();
+
+                switch (int.Parse(option))
+                {
+                    case 1:
+                        InputStudent();
+                        break;
+                    case 2:
+                        DisplayStudents();
+
+                        break;
+                    case 3:
+                        SearchStudents();
+                        break;
+                    case 4:
+                        inputtingStudent = false;
+                        break;
+                }
+            }
+            static void DisplayStudents(IEnumerable<Student> students)
+            {
+                if (students.Any())
+                {
+                    Console.WriteLine($"Student Id | Name | Class ");
+                    studentsList.ForEach(x =>
+                    {
+                        Console.WriteLine(x.StudentDisplay);
+                    });
+                }
+                else
+                {
+                    System.Console.WriteLine("no students found.");
+                }
+            }
+
+            static void DisplayStudents() => DisplayStudents(studentsList);
+
+            static void SearchStudents()
+            {
+                Console.WriteLine("Search string:");
+                var searchString = Console.ReadLine();
+                var students = studentsList.Where(x => x.FullName.Contains(searchString));
+                DisplayStudents(students);
+            }
+            static void DisplayMenu()
+            {
+                Console.WriteLine("Select from the following operations:");
+                Console.WriteLine("1: Enter new student");
+                Console.WriteLine("2: List all students");
+                Console.WriteLine("3: Search for student by name");
+                Console.WriteLine("4: Exit");
+            }
+
+            static void InputStudent()
+        {
+            var student = new Student();
+            // Continue prompting the user for input until it is valid
+            while (true)
+            {
+                // Prompt user
+                Console.WriteLine("Enter Student Id");
+                // Try to parse the user input 
+                var studentIdSuccessful = int.TryParse(Console.ReadLine(), out var studentId);
+                // If the input is valid 
+                if (studentIdSuccessful)
+                {
+                    // Add input to the Student object 
+                    student.StudentId = studentId;
+                    // Exit the loop
+                    break;
+                }
+            }
+            Console.WriteLine("Enter First Name");
+            var studentFirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            var studentLastName = Console.ReadLine();
+            Console.WriteLine("Enter Class Name");
+            var className = Console.ReadLine();
+            Console.WriteLine("Enter Last Class Completed");
+            var lastClass = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Enter Last Class Completed Date in format MM/dd/YYYY");
+                var lastCompletedOnSuccessful = DateTimeOffset.TryParse(Console.ReadLine(), out var lastClassCompletedOn);
+                if (lastCompletedOnSuccessful)
+                {
+                    student.LastClassCompletedOn = lastClassCompletedOn;
+                    break;
+                }
+            }
+            while (true)
+            {
+                Console.WriteLine("Enter Start Date in format MM/DD/YYYY");
+                var startDateSuccessful = DateTimeOffset.TryParse(Console.ReadLine(), out var startDate);
+                if (startDateSuccessful)
+                {
+                    student.StartDate = startDate;
+                    break;
+                }
+            }
+            studentsList.Add(student);
+        }
+        }
+    }
+    }
